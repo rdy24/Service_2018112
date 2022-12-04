98fb7c07c7cbd4bd337ec8a6ee847b61ac813701
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2022-12-04 18:45:14 +0700

    initial commit

---
+++ app/src/main/java/com/example/mymusicapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mymusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.mymusicapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+  int i = 1;
+  ActivityMainBinding binding;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    setTitle("2018112_Service Music Player");
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    binding.btnPlay.setOnClickListener(view -> startService(new Intent(MainActivity.this, MusicService.class)));
+
+    binding.btnNext.setOnClickListener(view -> {
+      if (i > 6) {
+        i = 0;
+      }
+      Intent intent = new Intent(MainActivity.this, MusicService.class);
+      intent.putExtra("position", i++);
+      startService(intent);
+    });
+
+    binding.btnPrev.setOnClickListener(view -> {
+      if (i < 0) {
+        i = 6;
+      }
+      Intent intent = new Intent(MainActivity.this, MusicService.class);
+      intent.putExtra("position", i--);
+      startService(intent);
+    });
+
+    binding.btnPause.setOnClickListener(view -> {
+      if (MusicService.mediaPlayer.isPlaying()) {
+        MusicService.mediaPlayer.pause();
+        binding.btnPause.setText("Continue");
+      } else {
+        MusicService.mediaPlayer.start();
+        binding.btnPause.setText("Pause");
+      }
+    });
+
+    binding.btnStop.setOnClickListener(view -> stopService(new Intent(MainActivity.this, MusicService.class)));
+  }
+}
---
+++ app/src/main/java/com/example/mymusicapp/MusicService.java
@@ -0,0 +1,47 @@
+package com.example.mymusicapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class MusicService extends Service {
+  static MediaPlayer mediaPlayer;
+
+  int[] music = {
+      R.raw.wildwest,
+      R.raw.battlesumeru,
+      R.raw.ormosgenshin,
+      R.raw.scaramouse,
+      R.raw.sumerucity, R.raw.sanctuary,
+          R.raw.wanderer,
+  };
+
+  @Nullable
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
+
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    int position = intent.getIntExtra("position", 0);
+    if (mediaPlayer != null) {
+      mediaPlayer.stop();
+      mediaPlayer.release();
+    }
+
+    mediaPlayer = MediaPlayer.create(this, music[position]);
+    mediaPlayer.setLooping(true);
+    mediaPlayer.start();
+    return START_STICKY;
+  }
+
+  @Override
+  public void onDestroy() {
+    mediaPlayer.stop();
+    super.onDestroy();
+  }
+}
